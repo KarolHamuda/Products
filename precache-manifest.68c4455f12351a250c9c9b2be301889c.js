05df491552eb8536e3aa93e60ef43edacbd370a5
Nemerianos <karol.hamuda@gmail.com>
2019-02-22 11:09:17 +0100

    Updates

---
+++ precache-manifest.68c4455f12351a250c9c9b2be301889c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a6fe9c583493591826e",
+    "url": "/Products/static/css/main.7b901b92.chunk.css"
+  },
+  {
+    "revision": "4a6fe9c583493591826e",
+    "url": "/Products/static/js/main.4a6fe9c5.chunk.js"
+  },
+  {
+    "revision": "cb1887c4752baf3d1b04",
+    "url": "/Products/static/js/runtime~main.cb1887c4.js"
+  },
+  {
+    "revision": "c6189fb5240967ea1725",
+    "url": "/Products/static/js/2.c6189fb5.chunk.js"
+  },
+  {
+    "revision": "9066fe5c46a8c8bf3618ae909e299c73",
+    "url": "/Products/index.html"
+  }
+];
